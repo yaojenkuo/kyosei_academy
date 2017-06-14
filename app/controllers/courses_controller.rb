d32f7e7f6166a512a1ac9e9f2b0169f5edf100af
yaojenkuo <tonykuoyj@gmail.com>
2017-06-14 15:27:26 +0800

    course CRUD

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,59 @@
+class CoursesController < ApplicationController
+    before_action :find_course, only: [:edit, :update, :destroy, :show]
+    
+    def index
+        @courses = Course.all
+    end
+
+    def show
+    end
+
+    def new
+        @course = Course.new
+    end
+
+    def create
+        @course = Course.new(course_params)
+        if @course.save
+            redirect_to courses_path, notice: "新增課程成功！"
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+    
+    def update
+        if @course.update_attributes(course_params)
+            redirect_to course_path, notice: "課程更新成功！"
+        else
+            render :edit
+        end
+    end
+
+    def create
+        @course = Course.new(course_params)
+
+        if @course.save
+            redirect_to courses_path, notice: "成功新增課程！"
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @course.destroy if @course
+        redirect_to courses_path, notice: "課程資料已刪除！"
+    end
+
+    private
+    def course_params
+        params.require(:course).permit(:name_en, :name_ch, :description)
+    end
+
+    def find_course
+      @course = Course.find_by(id: params[:id])
+    end
+    
+end
